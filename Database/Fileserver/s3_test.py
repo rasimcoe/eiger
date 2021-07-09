93fcc4d7c9c202328b966774c4da7a71f3ca8a72
Robert Simcoe <simcoe@Roberts-MBP-9.fios-router.home>
2021-07-08 21:10:40 -0400

    Committed S3 with fileserver download

--- Database/Fileserver/s3_test.py
+++ Database/Fileserver/s3_test.py
@@ -3,13 +3,19 @@ import boto3
 import os
 
 # Get the appropriate local credentials
-s3_resource = boto3.client('s3')
+s3_client   = boto3.client('s3')
+s3_resource = boto3.resource('s3')
 
 try:
-    reply = s3_resource.list_buckets()
-    # reply = s3_resource.list_objects(Bucket='gto1243')
+    reply = s3_client.list_buckets()
     print(reply['Buckets'])
 
+    remote_filename = 'QuasarSpectra/FIRE/ULAS1120_F.fits'
+    local_filename  = 'ULAS1120_F.fits'
+    
+    reply = s3_resource.Bucket('gto1243').\
+        download_file(remote_filename, local_filename)
+    
 except Exception as e:
     print("Fileserver connection failed: {}".format(e))                
                 
