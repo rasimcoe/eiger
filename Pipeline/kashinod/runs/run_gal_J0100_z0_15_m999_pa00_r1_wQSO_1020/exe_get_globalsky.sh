9bb7591692869d0382c3ebc978773fb9426f7c15
Daichi Kashino <61237761+kashinod@users.noreply.github.com>
2022-03-29 17:35:14 +0900

    Add files via upload

---
+++ Pipeline/kashinod/runs/run_gal_J0100_z0_15_m999_pa00_r1_wQSO_1020/exe_get_globalsky.sh
@@ -0,0 +1,47 @@
+# python ../scripts/get_globalsky.py list_nrca1_f115w.txt nrca1_f115w
+# python ../scripts/get_globalsky.py list_nrca1_f200w.txt nrca1_f200w
+# python ../scripts/get_globalsky.py list_nrca2_f115w.txt nrca2_f115w
+# python ../scripts/get_globalsky.py list_nrca2_f200w.txt nrca2_f200w
+# python ../scripts/get_globalsky.py list_nrca3_f115w.txt nrca3_f115w
+# python ../scripts/get_globalsky.py list_nrca3_f200w.txt nrca3_f200w
+# python ../scripts/get_globalsky.py list_nrca4_f115w.txt nrca4_f115w
+# python ../scripts/get_globalsky.py list_nrca4_f200w.txt nrca4_f200w
+# python ../scripts/get_globalsky.py list_nrca5_f356w_imaging.txt nrca5_f356w_imaging
+# python ../scripts/get_globalsky.py list_nrcb1_f115w.txt nrcb1_f115w
+# python ../scripts/get_globalsky.py list_nrcb1_f200w.txt nrcb1_f200w
+# python ../scripts/get_globalsky.py list_nrcb2_f115w.txt nrcb2_f115w
+# python ../scripts/get_globalsky.py list_nrcb2_f200w.txt nrcb2_f200w
+# python ../scripts/get_globalsky.py list_nrcb3_f115w.txt nrcb3_f115w
+# python ../scripts/get_globalsky.py list_nrcb3_f200w.txt nrcb3_f200w
+# python ../scripts/get_globalsky.py list_nrcb4_f115w.txt nrcb4_f115w
+# python ../scripts/get_globalsky.py list_nrcb4_f200w.txt nrcb4_f200w
+# python ../scripts/get_globalsky.py list_nrcb5_f356w_imaging.txt nrcb5_f356w_imaging
+
+
+#python ../scripts/get_globalsky.py list_nrca5_f356w_imaging.txt nrca5_f356w_imaging_masked mask_list_nrca5_f356w_imaging.txt
+#python ../scripts/get_globalsky.py list_nrcb5_f356w_imaging.txt nrcb5_f356w_imaging_masked mask_list_nrcb5_f356w_imaging.txt
+
+#python ../scripts/get_globalsky.py list_nrca1_f115w.txt nrca1_f115w_masked mask_list_nrca1_f115w.txt
+#python ../scripts/get_globalsky.py list_nrca2_f115w.txt nrca2_f115w_masked mask_list_nrca2_f115w.txt
+#python ../scripts/get_globalsky.py list_nrca3_f115w.txt nrca3_f115w_masked mask_list_nrca3_f115w.txt
+#python ../scripts/get_globalsky.py list_nrca4_f115w.txt nrca4_f115w_masked mask_list_nrca4_f115w.txt
+#python ../scripts/get_globalsky.py list_nrcb1_f115w.txt nrcb1_f115w_masked mask_list_nrcb1_f115w.txt
+#python ../scripts/get_globalsky.py list_nrcb2_f115w.txt nrcb2_f115w_masked mask_list_nrcb2_f115w.txt
+#python ../scripts/get_globalsky.py list_nrcb3_f115w.txt nrcb3_f115w_masked mask_list_nrcb3_f115w.txt
+#python ../scripts/get_globalsky.py list_nrcb4_f115w.txt nrcb4_f115w_masked mask_list_nrcb4_f115w.txt
+
+#python ../scripts/get_globalsky.py list_nrca1_f200w.txt nrca1_f200w_masked mask_list_nrca1_f200w.txt
+#python ../scripts/get_globalsky.py list_nrca2_f200w.txt nrca2_f200w_masked mask_list_nrca2_f200w.txt
+#python ../scripts/get_globalsky.py list_nrca3_f200w.txt nrca3_f200w_masked mask_list_nrca3_f200w.txt
+#python ../scripts/get_globalsky.py list_nrca4_f200w.txt nrca4_f200w_masked mask_list_nrca4_f200w.txt
+#python ../scripts/get_globalsky.py list_nrcb1_f200w.txt nrcb1_f200w_masked mask_list_nrcb1_f200w.txt
+#python ../scripts/get_globalsky.py list_nrcb2_f200w.txt nrcb2_f200w_masked mask_list_nrcb2_f200w.txt
+#python ../scripts/get_globalsky.py list_nrcb3_f200w.txt nrcb3_f200w_masked mask_list_nrcb3_f200w.txt
+#python ../scripts/get_globalsky.py list_nrcb4_f200w.txt nrcb4_f200w_masked mask_list_nrcb4_f200w.txt
+
+#python ../scripts/get_globalsky.py list_nrca5_f356w_wfss.txt nrca5_f356w_wfss
+#python ../scripts/get_globalsky.py list_nrcb5_f356w_wfss.txt nrcb5_f356w_wfss
+
+# 2022.03.29
+python ../scripts/get_globalsky.py list_img2_wfss_medFiltered_withMask_nrca5.txt medFiltered_withMask_nrca5 mask_list_nrca5_f356w_wfss.txt
+python ../scripts/get_globalsky.py list_img2_wfss_medFiltered_withMask_nrcb5.txt medFiltered_withMask_nrcb5 mask_list_nrcb5_f356w_wfss.txt
