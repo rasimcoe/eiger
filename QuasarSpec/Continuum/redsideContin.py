7ccaa358f328b87a08aec93b732a8a457cb3aa26
Rob Simcoe <simcoe@Roberts-MBP-9.fios-router.home>
2022-03-30 08:48:01 -0400

    Unsatisfying changes to the continuum fitting procedures

--- QuasarSpec/Continuum/redsideContin.py
+++ QuasarSpec/Continuum/redsideContin.py
@@ -7,7 +7,7 @@ from eiger.QuasarSpec.loadQsoSpec import loadQsoSpec
 import os
 from astropy.table import Table
 
-def redsideContin(specdict, instrument, writefile=False):
+def redsideContin(specdict, instrument, writefile=False, bspline=True):
 
     spec = specdict[instrument]
     redshift = specdict['z_em']
@@ -23,9 +23,10 @@ def redsideContin(specdict, instrument, writefile=False):
     #############
     #
     # Mask out known problem areas
+    #
     fitmask[restwave < 1210] = False
-    fitmask[np.logical_and(wave > 13590, wave < 14100)] = False
-    fitmask[np.logical_and(wave > 17900, wave < 19400)] = False
+    fitmask[np.logical_and(wave > 13490, wave < 14300)] = False
+    fitmask[np.logical_and(wave > 17900, wave < 19500)] = False
 
     if (instrument=='MOSFIRE_Y'):
         fitmask[np.logical_or(wave < 9700, wave > 10800)] = False
@@ -35,55 +36,93 @@ def redsideContin(specdict, instrument, writefile=False):
         fitmask[np.logical_or(wave < 14700, wave > 17900)] = False
     if (instrument=='MOSFIRE_K'):
         fitmask[wave < 19520] = False
+
+    if (specdict['objname']=='J1030+0524' or specdict['objid']==1):
+        fitmask[np.logical_and(wave > 8990,wave < 9024)] = False
+        fitmask[np.logical_and(wave > 15600,wave < 15700)] = False
+        fitmask[np.logical_and(wave > 10540,wave < 10620)] = False
+    if (specdict['objname']=='P159-02' or specdict['objid']==2):
+        fitmask[np.logical_and(wave > 13040,wave < 13100)] = False
+    if (specdict['objname']=='J0100+2802' or specdict['objid']==4):
+        fitmask[np.logical_and(wave > 14600,wave < 14660)] = False
+        fitmask[np.logical_and(wave > 9450,wave < 9500)] = False
+        fitmask[np.logical_and(wave > 9800,wave < 9850)] = False
+    if (specdict['objname']=='J0148+0600' or specdict['objid']==6):
+        fitmask[np.logical_and(wave > 9715,wave < 9765)] = False
+        
     #
     #####################
         
-    # 0. Divide by a smoothed spectrum to take out large departures from
-    #    the sensitivity functions
+    # 0. Divide by a bspline spaced at every "binwidth" pixels to
+    # smooth out any large scale variations caused by narrow emission
+    # lines or dodgy sensitivity functions in pypeit reductions.
+    # Rather than a median, this fits intentionally a bit high (upper
+    # 75th %-ile) because that screens out broad absorption lines a bit better.
+    # We will fix that systematic difference further below in iterative fit.
 
-    binwidth  = 150
+    binwidth  = 250
     halfwidth = binwidth // 2
     nbins     = (naxis1 // binwidth) - 1
     indx = halfwidth + np.arange(nbins) * binwidth
     gd = (fitmask[indx] == True)
     xbin = restwave[indx[gd]]
 
-    # Note this intentionally fits high (70 %-ile)
+    # Note this intentionally fits high (75 %-ile)
     # to eliminate dipping into absorption lines
-    ybin = np.array([np.quantile(flux[ii-halfwidth:ii+halfwidth],0.70) for ii in indx[gd]])
+    ybin = np.array([np.quantile(flux[ii-halfwidth:ii+halfwidth],0.75) for ii in indx[gd]])
 
-    # Screen bad areas and save points for later
+    # Tidy up some areas we won't use, and save points for later
     ybin[xbin < 1216] = ybin[xbin > 1216][0]
     ybin[ybin < 1e-20] = np.median(ybin)
     xbin1 = xbin
     ybin1 = ybin
 
-    # Fit the knots with a bspline and normalize
+    # Fit the knots with a bspline and normalize it out
     t, c, k = interpolate.splrep(xbin, ybin, k=3, s=0)
     flux_filt = interpolate.BSpline(t,c,k)
+
     flux = flux / flux_filt(restwave)
+    ivar = ivar * (flux_filt(restwave))**2
+    fitmask[flux*np.sqrt(ivar) < 3] = False
 
+    
     ##################################
+    #
+    # 1. Instatiate a mask for points to include in the fit by
+    # refitting an interpolated function to the normalized spectrum
+    # As before, bias the fit a bit high becuase we want to avoid dipping
+    # into real absorption lines.
 
-    binwidth  = 200
+    if (instrument=='HIRES'):
+        binwidth=250
+    else:
+        binwidth  = 60
+
     halfwidth = binwidth // 2
     nbins     = (naxis1 // binwidth) - 1
     indx = halfwidth + np.arange(nbins) * binwidth
     gd = (fitmask[indx] == True)
-    xbin = restwave[indx[gd]]
-    
-    # 1. Calculate an upper envelope
 
-    ybin = [np.quantile(flux[ii-halfwidth:ii+halfwidth],0.75) for ii in indx[gd]]
+    xbin = np.array(restwave[indx[gd]])
+    ybin = np.array([np.quantile(flux[ii-halfwidth:ii+halfwidth],0.75) for ii in indx[gd]])
+
     flux_filt2 = interpolate.interp1d(xbin, ybin, bounds_error=False, \
                                      fill_value='extrapolate')
 
     pk_mask, _ = find_peaks(flux,height=(flux_filt2(restwave),None))
 
-    # With successive iterations, tighten the criteria to keep points
+    
+    ######################################
+    #
+    # 2. Now we have pk_mask, which contains the points we will use.
+    # Next is to iteratively fit and reject outliers using RANSAC
+    
+    # Set the number of iterations and rejection criteria, which
+    # vary according to which spectrograph is used.
+    
     if (instrument == 'MOSFIRE'):
         niter=3
-        discard = [5,4,3]
+        discard = [4,3,2]
     elif (instrument == 'FIRE' or instrument == 'XSH_NIR'):
         niter=3
         discard = [4,3,2]
@@ -91,64 +130,89 @@ def redsideContin(specdict, instrument, writefile=False):
         niter=3
         discard = [4,3,2]
     else:
-        niter=2
-        discard = [4,3]
+        niter=1
+        discard = [10,5]
 
-    bspline = True
-
-    # Iteratively reject outliers while fitting a bspline continuum
+    cont = np.ones(len(restwave))
+        
     for i in range(niter):
     
         if (i == 0):
+            # f is the new continuum model, on first pass it is a fit
+            # to the peaks found above
             f = interpolate.interp1d(restwave[pk_mask],flux[pk_mask],\
                                      bounds_error=False, \
                                      fill_value='extrapolate')
+            inmask = fitmask
         else:
+            # On 2nd and later passes, f is taken from the previous fit.
             f = c
-        
+            inmask = inlier_mask
+            
         # 3. Apply RANSAC to detect outlying features (esp. absorption) and mask
 
-        mad = np.median(np.abs(np.median(flux)-flux))
-
+        inmask_ind = np.where(inmask)[0]
+        mad = np.median(np.abs(np.median(flux[inmask_ind]/cont[inmask_ind])-flux[inmask_ind]/cont[inmask_ind]))
+        # print("MAD = {}".format(mad))
+        
         ransac = linear_model.RANSACRegressor(random_state=0, \
                                               loss='absolute_loss',\
                                               residual_threshold=discard[i]*mad)
 
-        ransac.fit(restwave.reshape(len(restwave),1), flux, \
-                   sample_weight=np.abs(ivar))
+        fitwv = restwave[inmask_ind]
+        fitflux = flux[inmask_ind]/cont[inmask_ind]
+        fitivar = ivar[inmask_ind]/cont[inmask_ind]
+        ransac.fit(fitwv.reshape(len(fitwv),1), fitflux, \
+                   sample_weight=fitivar)
+        #ransac.fit(restwave.reshape(len(restwave),1), flux, \
+        #           sample_weight=np.abs(ivar))
 
-        inlier_mask  = ransac.inlier_mask_
+        inlier_mask  = fitmask
+        # inlier_mask[inmask_ind[:]] = ransac.inlier_mask_[:]
+
+        # return(ransac.inlier_mask_)
+        
+        # Recall that fitmask contains only the regions we masked by hand
+        # We need to be sure to re-eliminate them at each iteration
+        inlier_mask[fitmask == False] = False
         inlier_mask[np.logical_and(restwave > 1215, restwave < 1230)] = True
-        inlier_mask[np.logical_and(wave > 13590, wave < 14100)] = False
-        inlier_mask[np.logical_and(wave > 17900, wave < 19400)] = False
-        inlier_mask[restwave < 1215] = False
-
-        if (instrument=='MOSFIRE_Y'):
-            inlier_mask[np.logical_or(wave < 9700, wave > 10800)] = False
-        if (instrument=='MOSFIRE_J'):
-            inlier_mask[np.logical_or(wave < 11500, wave > 13550)] = False
-        if (instrument=='MOSFIRE_H'):
-            inlier_mask[np.logical_or(wave < 14700, wave > 17900)] = False
-        if (instrument=='MOSFIRE_K'):
-            inlier_mask[wave < 19520] = False
         
         # 4. Smooth the inlier data points to build the continuum
 
-        if (bspline):
-            gd = (fitmask[indx] == True)
-            ybin = [np.median(flux[ii-halfwidth:ii+halfwidth]) for ii in indx[gd]]
-            t, c, k = interpolate.splrep(xbin, \
-                                         ybin, \
-                                         k=3, s=0)
-            c = interpolate.BSpline(t,c,k)
+        gd = (fitmask[indx] == True)
+        xbin = np.zeros(len(indx[gd]))
+        ybin = np.zeros(len(indx[gd]))
+        for i in range(len(indx[gd])):
+            ii = (indx[gd])[i]
+            xbin[i] = restwave[ii]
+            medpoints = flux[ii-halfwidth:ii+halfwidth]
+            medmask   = inlier_mask[ii-halfwidth:ii+halfwidth]
+            ybin[i] = np.median(medpoints[medmask])
+
             
+        use = ~np.isnan(ybin)
+        if (bspline):
+            t, c, k = interpolate.splrep(xbin[use], ybin[use], k=3, s=0)
+            c = interpolate.BSpline(t,c,k)
         else:
             c = interpolate.interp1d(restwave[inlier_mask],\
                                      medfilt(flux[inlier_mask],kernel_size=51), \
                                      fill_value='extrapolate')
 
-        cont = c(restwave)
 
+        cont = c(restwave)
+        if (False):
+            plt.plot(fitwv,fitflux)
+            plt.plot(fitwv[ransac.inlier_mask_==False],fitflux[ransac.inlier_mask_==False],'+')
+            plt.plot(restwave,cont)
+            #plt.plot(restwave[inlier_mask==False],flux[inlier_mask==False], '+')
+            #plt.plot(xbin,ybin,'o',color='y',markersize=3)
+            plt.ylim(-3,5)
+            plt.show()
+
+
+    # Go back and put back in the crude normalization that we did at the
+    # start of the process, so that the output is the same as the input flux
     cont = cont * flux_filt(restwave)
     cont[restwave < 1216] = (cont[restwave > 1216])[0]
     flux = flux * flux_filt(restwave)
@@ -159,13 +223,14 @@ def redsideContin(specdict, instrument, writefile=False):
 #        plt.plot(wave,flux_filt(restwave))
 #        plt.plot(xbin1 * (1+redshift),ybin1,'+')
 #        plt.plot(wave[inlier_mask==False],flux[inlier_mask==False], '+')
+        plt.plot(xbin[use]*(1+redshift),ybin[use]*flux_filt(xbin[use]), 'o',color='y',markersize=3)
         plt.ylim(-1.0*np.median(cont),3*np.median(cont))
         plt.show()
 
     # Write the output to disk as a fits binary table
     # in the same directory as the data file
     if (writefile):
-        infile   = loadQsoSpec(specdict['objid'], files=True)
+        infile   = loadQsoSpec(specdict['objid'],revision=specdict['revision'],files=True)
         path     = os.path.dirname(infile[instrument])
         contname = os.path.basename(infile[instrument])[:-5]+'_contin.fits'
         outfile  = path+'/'+contname
@@ -173,7 +238,7 @@ def redsideContin(specdict, instrument, writefile=False):
         t.write(outfile, format='fits', overwrite=True)
         print(f"Writing: {outfile}")
         
-    return(cont)
+    return(xbin[use]*(1+redshift),ybin[use])
 
 
 def allContin(writefile=False):
@@ -195,7 +260,9 @@ def allContin(writefile=False):
     qso = loadQsoSpec(3)
     firecontin      = redsideContin(qso,'FIRE',writefile=writefile)
     xsh_nir_contin  = redsideContin(qso,'XSH_NIR',writefile=writefile)
-    xsh_vis_contin  = redsideContin(qso,'XSH_VIS',writefile=writefile)
+
+    # The VIS reduction of 1120 looks correupted somehow
+    # xsh_vis_contin  = redsideContin(qso,'XSH_VIS',writefile=writefile)
 
     # 4 = J0100+2802
     qso = loadQsoSpec(4)
